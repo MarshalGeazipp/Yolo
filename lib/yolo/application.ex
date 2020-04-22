da8b0ad068c1821ef6ca4510bdd98484e5d229a3
Sebastian Nicklas <sebastian.nicklas@shopviu.com>
2020-04-22 22:38:31 +0200

    Initial commit. Object detection in uploaded image works

---
+++ lib/yolo/application.ex
@@ -0,0 +1,29 @@
+defmodule Yolo.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Start the endpoint when the application starts
+      YoloWeb.Endpoint,
+      # Starts a worker named Yolo.Worker
+      {Yolo.Worker, [name: Yolo.Worker]}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Yolo.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    YoloWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
